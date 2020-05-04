5373ddfbe93870922541a1a9d40088f7e21d44cd
Crystal Ly <crystal.m.ly@gmail.com>
2020-05-04 15:59:54 -0500

    set position of hands

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+var date = new Date();
+
+console.log(date);
+
+let hour = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+
+// Convert the hour, minutes, and seconds into degrees for the position of the hands.
+
+let hrPosition = (hour * 360 / 12) + ((minutes * 360 / 60) / 12);
+let minPosition = (minutes * 360 / 60) + (sec * (360 / 60) / 60);
+let secPosition = seconds * (360 / 60);
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
